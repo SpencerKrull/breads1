f5ffb6853658811344d75142ac425a1673c62ced
Spencer Krull <spencerkrull138@gmail.com>
2022-06-07 18:08:36 -0600

    sending index and show data

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { append } = require('express/lib/response')
+
+//CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Bread')
+})
+
+//LISTEN
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT);
+})
+
+//Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
