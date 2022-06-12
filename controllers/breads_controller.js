53f84189536ae24ac0593e383ed5bcac138212ae
Spencer Krull <spencerkrull138@gmail.com>
2022-06-12 10:54:17 -0600

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -12,12 +12,17 @@ breads.get('/', (req, res) => {
   // res.send(Bread)
 })
 
-
-module.exports = breads
-
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
+})
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
 })
 
 // SHOW
@@ -32,7 +37,6 @@ breads.get('/:arrayIndex', (req, res) => {
     }
 })
 
-// CREATE
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
@@ -52,3 +56,16 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+module.exports = breads
