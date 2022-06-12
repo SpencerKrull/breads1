b075c003807315c0ab3f3771bd19addcc911e437
Spencer Krull <spencerkrull138@gmail.com>
2022-06-12 09:26:33 -0600

    delete route complete

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { append } = require('express/lib/response')
 
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 //CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -12,6 +15,8 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 
 // ROUTES
 app.get('/', (req, res) => {
